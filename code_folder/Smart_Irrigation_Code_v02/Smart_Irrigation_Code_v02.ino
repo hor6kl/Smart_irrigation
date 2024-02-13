7b13b8fd56994cdd064b3fcf4ecc262ae0f8f1e9
hor6kl <horakl@proton.me>
2024-02-13 19:24:22 +0100

    Functional setup, non tested code

--- code_folder/Smart_Irrigation_Code_v02/Smart_Irrigation_Code_v02.ino
+++ code_folder/Smart_Irrigation_Code_v02/Smart_Irrigation_Code_v02.ino
@@ -2,24 +2,83 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // nastav adresu LDC na 0x27 pro 16 sloupcovy a 2 radkovy displej
 
+// Pin variables
 int relayPin_mot = 7;
 int relayPin_hum = 4;
-int sensorPowerPin = 8;
 
 int sensor_pin_hum = A0;
-
-int output_value_hum ;
-int starttime;
 
 int buttonPin = 12;
 int ledPin = 13;
 
-Sleep sleep;
-unsigned long sleepduration; /*duration for which the Arduino needs to be in sleep mode*/
+
+int output_value_hum ;
 
 // variables will change:
 int buttonState = 0;
+
+// variables for humidity sensor
+int water_value = 1000;
+int dirt_value = 100;
+
+// variables for time loop
+const uint32_t pauza = 10000;  //10 s
+unsigned long starttime;
+unsigned long starttime_2;
+unsigned long currenttime;
+unsigned long end_time_2;
+
+// Funtion for display
+
+unsigned long casovaZnacka;
+
+void obrazovka1(int hum, long allSeconds)
+{
+
+int runHours= allSeconds/3600;
+int secsRemaining=allSeconds%3600;
+int runMinutes=secsRemaining/60;
+int runSeconds=secsRemaining%60;
+
+
+  
+  lcd.begin(16,2);
+  lcd.clear();
+
+  lcd.setCursor(1, 0);
+  lcd.print("Vlhkost:");  
+  lcd.setCursor(10, 0);
+  lcd.print(hum);
+
+  lcd.setCursor(0, 1);
+  lcd.print("H:");
+  lcd.setCursor(2, 1);
+  lcd.print(runHours);
+  lcd.setCursor(5, 1);
+  lcd.print("M:");  
+  lcd.setCursor(7, 1);
+  lcd.print(runMinutes);
+  lcd.setCursor(10, 1);
+  lcd.print("S:");   
+  lcd.setCursor(12, 1);
+  lcd.print(runSeconds); 
+}
+
+void obrazovka2()
+{
+
+    lcd.begin(16,2);
+    lcd.clear();
+    lcd.setCursor(4, 0);
+    lcd.print("EMERGENCY");
+    lcd.setCursor(4, 1);
+    lcd.print("SHUT-DOWN");
+
+
+}
+
 
 
 void setup ()
@@ -32,76 +91,107 @@ void setup ()
 
   pinMode(sensor_pin_hum, INPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
   Serial.println ("Reading From the Sensor ...");
 
 
   lcd.init();           // initialize the lcd
   lcd.backlight();      // zapni podsvícení
-  lcd.setCursor(4, 0);  // dej kurzor na sloupec 1, řádek 0
-  lcd.print("ARDUINO"); // napis na displej "ARDUINO"
-  delay(pauza);
-  casovaZnacka = millis(); // vytvori casovou znacku
+  lcd.setCursor(3, 0);  // dej kurzor na sloupec 1, řádek 0
+  lcd.print("IRRIGATION"); // napis na displej "ARDUINO"
+  delay(2000);
+  starttime = millis(); // vytvori casovou znacku
 
 }
 
 
 
 void loop() {
+  unsigned long allSeconds=millis()/1000;
 
-  output_value = analogRead(sensor_pin);
-
-  output_value = map (output_value, 550, 10, 0, 100);
-
-  Serial.print ("Moisture : ");
-  Serial.print (output_value);
-  Serial.print ("%");
-  Serial.print('\n');
-
-  digitalWrite (sensorPowerPin, HIGH);
+  buttonState = digitalRead(buttonPin);
 
 
-// variables will change:
-  if (output_value < 32){
+//  Serial.print ("Moisture : ");
+//  Serial.print (output_value_hum);
+//  Serial.print('\n');
 
-    do {
-      starttime = millis();
-      delay(1250);
+//  Serial.print(currenttime);
+  
+  // time loop 
+  if (millis() - currenttime > 3600000) {
 
-      output_value = analogRead(sensor_pin);
+//      digitalWrite (relayPin_mot, HIGH);
+      digitalWrite (relayPin_hum, LOW); 
+
+      delay(20000);
+      output_value_hum = analogRead(sensor_pin_hum);
+
+
+      if (output_value_hum > 430){
+        
+        starttime_2 = millis();
+        end_time_2 = millis();
+        
+        do {
+         
+          delay(1250);
+
+          output_value_hum = analogRead(sensor_pin_hum);
+           
+          digitalWrite (relayPin_mot, LOW);
+          delay(2000);
+          digitalWrite (relayPin_mot, HIGH);          
+
     
-      output_value = map (output_value, 550, 10, 0, 100);
-       
-      digitalWrite (relayPin, LOW);
+          Serial.print ("Moisture : ");
+          Serial.print (output_value_hum);
+          Serial.print (" --> ON");
+          Serial.print('\n');
 
-      Serial.print ("Moisture : ");
-      Serial.print (output_value);
-      Serial.print (" --> ON");
-      Serial.print('\n');
+    
+          if (output_value_hum > 350 && (end_time_2 - starttime_2) > 50000){
+            digitalWrite (relayPin_mot, HIGH);
+            digitalWrite (relayPin_hum, HIGH);
+            
+            Serial.print ("Reset the system, moisture does not reached defined value in 50s. ");
+            while (true){
+              digitalWrite(LED_BUILTIN, HIGH);
+              delay(1000);
+              digitalWrite(LED_BUILTIN, LOW);
+              delay(1000);
+              obrazovka2();          
+              }
+            }
 
-      if (output_value < 65 && starttime > 100000){
-        digitalWrite (relayPin, HIGH);
-        Serial.print ("Reset the system, moisture does not reach defined value in 10s. ");
-        while (true){
-          digitalWrite(LED_BUILTIN, HIGH);
-          delay(1000);
-          digitalWrite(LED_BUILTIN, LOW);
-          delay(1000);          
-          }
-        }
+        end_time_2 = millis();
+            
+        } while (output_value_hum > 350);
+      }
+    
 
-    } while (output_value < 65);
+    currenttime = millis();
 
+      
+  }
+  else if (buttonState == 0){
+    digitalWrite (relayPin_mot, LOW);
+
+    obrazovka1(output_value_hum, currenttime/1000);
+    
+  }
+  
+  else {
+
+      digitalWrite (relayPin_mot, HIGH);
+      digitalWrite (relayPin_hum, HIGH); 
+      
+      obrazovka1(output_value_hum, currenttime/1000);
+//      delay(pauza);
+     
   }
 
-  else
-
-  {
-
-    digitalWrite (relayPin, HIGH);
-
-  }
 
 
 }
